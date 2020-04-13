68132939be283e3e1fe18b115565f3e203ce00c8
Jared Lam <lam.ja@husky.neu.edu>
2020-04-13 01:23:54 -0400

    fixes to pb

--- NightbotSpeedrunCommands.js
+++ NightbotSpeedrunCommands.js
@@ -1,25 +1,30 @@
 // -------------------------------------------------------------
-// !pb
+// !pbhelper
 // 4pd0n31e is the speedrun.com id for Portal 1
-
+// called from !pb which returns https://speedrun.com/api/v1/users/Zoochable/personal-bests https://speedrun.com/api/v1/games/Portal/categories
 $(eval 
-var api=$(urlfetch json https://speedrun.com/api/v1/users/Zoochable/personal-bests);
-var cs=$(urlfetch json https://speedrun.com/api/v1/games/Portal/categories);
+var a=$(urlfetch json $(1));
+var cs=$(urlfetch json $(2));
 var cIs={};
-for (var c in cs['data']) {
-    var d = cs['data'][c];
+for(var c in cs['data']){
+    var d=cs['data'][c];
     cIs[d['id']]=d['name'];
-};
-var out= "My PBs: ";
-for (var pb in api['data']) {
-    var r=api['data'][pb]['run'];
-    if (r['game'] === "4pd0n31e") {
+}
+var out="My PBs: ";
+for(var pb in a['data']){
+    var r=a['data'][pb]['run'];
+    if(r['game'] === "4pd0n31e"){
         var t=r['times']['primary_t'];
-        out+=cIs[r['category']]+": "+Math.floor(t/60)+":"+((t*100)%6000)/100+" ";
+        var m=Math.floor(t/60);
+        var sc=Math.round((t-60*m)*100)/100+"";
+        var sm=sc.split('.');
+        var pd=`${sm[0].padStart(2,'0')}.${sm[1].padEnd(2,'0')}`;
+        out+=cIs[r['category']]+`: ${m}:${pd} `;
     }
-};
+}
 out
 )
+
 
 // --------------------------------------------------------------
 // !wrhelper
@@ -31,7 +36,8 @@ var player=$(urlfetch json $(1));
 var playername = player['data']['names']['international'];
 var minutes = Math.floor(wrtime/60);
 var seconds = Math.round((wrtime - 60 * minutes) * 100) / 100 + "";
-var padded = seconds.padStart(5, '0');
+var secsmilis = seconds.split('.');
+var padded = `${secsmilis[0].padStart(2,'0')}.${secsmilis[1].padEnd(2,'0')}`;
 `$(2) World Record is ${minutes}:${padded} by ${playername}`
 )
 
--- README.md
+++ README.md
@@ -10,11 +10,12 @@ Commands:
 
 To use with your stream:
 1. Add nightbot to your stream (google how to do it I'm to lazy to tell you right here)
-2. Setup commands !pb, !wr, !wrhelper with the messages as the respective javascript code
+2. Setup commands !pbhelper, !wr, !wrhelper with the messages as the respective javascript code (You might need to shrink !pbhelper by deleting all the newlines and indentation)
 3. Alias !wr to call !wrhelper (literally type !wrhelper in the alias box)
-4. Change Zoochable in !pb in the cs url to whatever your username is
-5. Change any appearance of 'portal' in any urls to whatever game that you run
-6. Change "4pd0n31e" to be the id of the game that you run
+4. Create a command !pb with the message `https://speedrun.com/api/v1/users/YOUR-USERNAME-HERE/personal-bests https://speedrun.com/api/v1/games/YOUR-GAME-HERE/categories` aliased to !pbhelper
+5. replace the username and game in those urls
+6. Change any appearance of 'portal' in any urls to whatever game that you run
+7. Change "4pd0n31e" to be the id of the game that you run
 
 It should theoretically work
 
